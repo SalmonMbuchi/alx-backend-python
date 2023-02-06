9cabdc3370e2d34170a2ed51d595e2a5c961a181
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-02-06 18:32:20 +0300

    return in order of completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """returns a list of delays"""
     myList = []
     newList = []
