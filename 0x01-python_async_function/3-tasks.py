4fb599e0352aa2c9f2592e9f69d7aa98a2f330e4
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-02-06 22:20:40 +0300

    return type of a class

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,10 +2,11 @@
 """Return a asyncio.Task"""
 
 import asyncio
+from typing import Type
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> object:
+def task_wait_random(max_delay: int) -> Type[asyncio.Task]:
     # num = asyncio.create_task(wait_random(max_delay))
     loop = asyncio.get_event_loop()
     return loop.create_task(wait_random(max_delay))
