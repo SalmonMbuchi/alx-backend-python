87b7de37ce873f75e4a037d9064c8bc6735c0435
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-02-06 16:26:08 +0300

    return a list of delays

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Execute multiple coroutines"""
+
+# import wait_random from './0-basic_async_syntax.py'
+import random
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    myList = []
+    for i in range(n):
+        num = await wait_random(max_delay)
+        myList.append(num)
+    return myList
