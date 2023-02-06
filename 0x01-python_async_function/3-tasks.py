9e4fb22523df3d4f5fc3d5636282f7733cef69c0
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-02-06 22:30:54 +0300

    return specific subclass

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,11 +2,13 @@
 """Return a asyncio.Task"""
 
 import asyncio
-from typing import Type
+from typing import Type, TypeVar, Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+O = TypeVar('O', bound=asyncio.Task)
 
-def task_wait_random(max_delay: int) -> Type[asyncio.Task]:
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
     # num = asyncio.create_task(wait_random(max_delay))
     loop = asyncio.get_running_loop()
     return loop.create_task(wait_random(max_delay))
